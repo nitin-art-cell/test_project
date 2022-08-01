b311396174d1c2ad731c579db6e4d4871b334463
nitin-art-cell <110383876+nitin-art-cell@users.noreply.github.com>
2022-08-01 02:07:59 -0700

    Add files via upload

---
+++ eretrey.java
@@ -0,0 +1,335 @@
+import java.util.Scanner;
+/*
+public class A {
+int a=1, b=2;
+static int c=3;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A ob=new A();
+System.out.println(ob.a+ob.b+c);
+ob.a=ob.a+c;
+ob.b=ob.a+ob.b;
+System.out.println(ob.a+ob.b+c);
+A ob1=new A();
+System.out.println(ob1.a+ob1.b+c);
+	}
+}            
+
+ /*  class A
+{
+	int a=1,b=2;
+	static int c=3;
+	public static void main()
+	{
+		A.ob=new A();
+		System.out.println(ob.a+ob.b+c);
+		ob.a=ob.b+c;
+		c=ob.a+ob.b;
+		System.out.println(ob.a+ob.b+c);
+		A ob1=new A();
+		System.out.println(ob.a+ob.b+c);
+	}
+}    */
+
+class AB
+{
+	int a=5,b=3,c=4;
+	static int d=5,e=6;
+	public static void main()
+	{
+		A ob=new A();
+		System.out.println(ob.a+ob.b+ob.c+d+e);
+		ob.a=ob.b+d;
+		ob.b=ob.c+e;
+		d=ob.a+ob.b;
+	System.out.println(ob.a+ob.b+ob.c+d+e);
+	A ob1=new A();
+	ob1.a=ob.a+ob.b;
+	ob1.b=ob1.a+ob1.b;
+	e=ob1.a+ob1.b;
+	System.out.println(ob1.a+ob1.b+ob1.c+d+e);
+	A ob2=new A();
+	System.out.println(ob2.a+ob2.b+ob2.c+d+e);
+	}
+}
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//System.out.println("hello mr. nitin");
+
+// write a program for the two no addition by the help of user input two no:
+		
+/*		int a, b;
+	   int c=a+b
+		System.out.println("taken no from the user");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println("please enter the first no");
+//		Scanner sc=new Scanner(System.in);
+//		String name=sc.next();
+		System.out.println("please enter the second no");
+		System.out.println("the result is:");  */
+		
+		
+/*	int no;
+	for(no=0; no<=5; no+1)
+	{
+		System.out.println(no);
+	}
+		
+		
+		
+// program of for loop in which we find the even no form 1 to 100:
+/*		int no;
+		for(no=2; no<=100 ; no=no+2)
+		{
+			System.out.println(no);
+		}  */
+		
+
+// program of for loop in which we find the odd no form 1 to 100:		
+	/*	int no;
+		for(no=1; no<=100 ; no=no+2)
+		{
+			System.out.println(no);
+		}  */
+	
+		
+/*		int no;
+		for(no=1; no<=10 ; no=no+1)
+		{
+			System.out.println(no);
+		}  */
+
+		
+	
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+		
+	/*	for(hello=0; hello<=7; hello+1)
+		{
+			System.out.println(hello);
+		}
+		
+		
+		/*		System.out.println("hello mister. please enter your lucky name");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println(" nice name "    +  name   +  " sir ");
+		System.out.println("nice to meet you");
+		
+		
+			System.out.println("please enter your lucky name");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println("  hii "  +  name);
+		System.out.println("have a good day");   */ 
+		
+		
+/*		
+//addition of two number:
+		
+		int a=10,b=20,c;
+ c=a+b;
+System.out.println(c);
+
+System.out.println("hello");
+		
+		
+
+// try and catch block with simple example:
+        
+        int=num1,num2
+		try 
+		{
+			num1=4;
+			num2=num1/0;
+			System.out.println(num2);
+			System.out.println("enter the first statement");
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("we can't devide with zero");
+		}
+		
+		
+//multiples catch block with example:		
+		
+		try
+		{
+			int a[]=new int[7];
+			a[4]=4/0;
+			System.out.println("print first statement");
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Exception warrning");
+		}
+		catch(ArrayIndexOutOfBoundsException a)
+		{
+			System.out.println("Exception warrning");
+		}
+		catch(Exception e)
+		{
+			System.out.println("all type exception");
+		}
+		
+		
+		
+		
+
+{
+	int a[]=new int[7];
+	a[4]=30/0;
+	System.out.println("print the first statement");
+}
+catch(ArithmeticException e)
+{
+	System.out.println("exception warrning");
+}
+catch(ArrayIndexOutOfBoundsException a)
+{
+	System.out.println("exception warrning");
+}
+catch(Exception e)
+{
+	System.out.println("some other exception");
+}
+		
+		
+int num1, num2;
+try 
+{
+	num1=2;
+	num2=num1/0;
+	System.out.println(num2);
+	System.out.println("end of the try block");
+}
+		catch(ArithmeticException e)
+{
+			System.out.println("you can't devide with zero");
+}
+	//	System.out.println("end of catch block");
+		
+		
+			int num1, num2;
+	try
+	{
+		num1=0;
+		num2=12/num1;
+		System.out.println("num2");
+		System.out.println("end or try block");
+	}
+		catch(ArithmeticException e)
+	{
+			System.out.println("you should't devide with zero");
+			
+	}
+
+	System.out.println("end of catch block");
+		
+		
+		
+			System.out.println("hello code village");
+
+	// 	input two no form the user:
+
+		System.out.println("taken from the user");
+		System.out.println("please enter the first no");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println("please enter the second no");
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("here is result");
+		System.out.println(c);
+		
+		
+		
+		for(int Counter=0; Counter<6; Counter++)
+		{
+			System.out.println("Counter");
+		}
+		
+			int a=10;
+		int b=0;
+		int c=a/b;
+		try
+		{
+			System.out.println("airthmeticexceptions e");
+		}
+		catch(airthmeticexception a)
+		{
+			System.out.println("e.getmessage");
+			System.out.println(c);
+		}
+		int d= (a+b+c);
+		System.out.println(d);  
+
+		System.out.println("please enter your lucky name");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println(" hii "  +  name  +  " have a good day ");  
+	
+		
+// find the no is Intiger or not:
+		System.out.println("enter your number for the find that no is intiger or not");
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println(sc.hasNextInt());  
+	
+		
+		// program of percentage:
+		
+		 int sub1=1000;
+		 int sub2=25;
+		 int sub3= (sub1 * sub2/100);
+   float per=(subject1 + subject2 + subject3)/3;
+   System.out.println(sub3);       	
+
+   
+   //  taking five no form the user at the program run time:
+   
+	System.out.println("taking input for the user");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter no 1");
+	int a=sc.nextInt();
+	System.out.println("enter no 2");
+	int b=sc.nextInt();
+	System.out.println("enter no 3");
+	int c=sc.nextInt();
+	System.out.println("enter no 4");
+	int d=sc.nextInt();
+	System.out.println("enter no 5");
+	int e=sc.nextInt();
+	
+	int result= a+b+c+d+e;
+	System.out.println("the result is");
+	System.out.println(result);    
+	
+// input two no from the user at the program run time:
+	
+	System.out.println("TAKING THE NO FROM THE USER");
+	System.out.println("please enter the first no");
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println("please enter the second no");
+	int b=sc.nextInt();
+	int c=a+b;
+	System.out.println("the result is");
+	System.out.println(c);  */  
+	
+//	}
+//}
